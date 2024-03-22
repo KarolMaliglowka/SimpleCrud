1ddb53184d8981e88850df594832b1acdb283fa1
MALIGLOK <karol.maliglowka@zf.com>
2024-03-22 22:44:03 +0100

    added angular using

--- src/SimpleCrud.Web/Program.cs
+++ src/SimpleCrud.Web/Program.cs
@@ -1,15 +1,27 @@
-using SimpleCrud.Application;
-using SimpleCrud.Infrastructure;
-using SimpleCrud.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddInfrastructure()
-    .AddApplication();
+// Add services to the container.
 
-var app = builder
-    .Build()
-    .UsePhoneApi();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
