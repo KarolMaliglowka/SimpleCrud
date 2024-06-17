d9434061cee609d1aa0ef70dce44aead6bf174e1
MALIGLOK <karol.maliglowka@zf.com>
2024-06-17 21:45:53 +0200

    changes

--- src/SimpleCrud.Application/Queries/GetPositionByNumber.cs
+++ src/SimpleCrud.Application/Queries/GetPositionByNumber.cs
@@ -1,6 +1,35 @@
-﻿namespace SimpleCrud.Application.Queries;
+﻿using SimpleCrud.Application.Abstractions;
+using SimpleCrud.Application.Dtos;
+using SimpleCrud.Core.Repositories;
 
-public class GetPositionByNumber
+namespace SimpleCrud.Application.Queries;
+
+public class GetPositionByNumber : IQuery<PhoneDto>
 {
+    public string PhoneNumebr { get; set; }
+}
 
+public class GetPositionByNumberHandler : IQueryHandler<GetPositionByNumber, PhoneDto>
+{
+    private readonly IPhoneBookRepository _phoneBookRepository;
+
+    public GetPositionByNumberHandler(IPhoneBookRepository phoneBookRepository)
+    {
+        _phoneBookRepository = phoneBookRepository;
+    }
+
+    public async Task<PhoneDto> HandleAsync(GetPositionByNumber query)
+    {
+        var result = await _phoneBookRepository.GetAsyncByPhoneNumber(query.PhoneNumebr);
+        
+        if (result is null)
+        {
+            throw new NullReferenceException();
+        }
+        
+        return new PhoneDto(
+            result.Id,
+            result.Name,
+            result.PhoneNumber);
+    }
 }
