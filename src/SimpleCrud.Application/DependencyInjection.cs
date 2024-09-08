0270359f2dd361bea7dd6ccd99296075e9a336f5
Karol <karol_maliglowka@o2.pl>
2024-09-08 18:19:10 +0200

    changed files names

--- src/SimpleCrud.Application/DependencyInjection.cs
+++ src/SimpleCrud.Application/DependencyInjection.cs
@@ -2,7 +2,7 @@
 
 namespace SimpleCrud.Application;
 
-public static class Extensions
+public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
