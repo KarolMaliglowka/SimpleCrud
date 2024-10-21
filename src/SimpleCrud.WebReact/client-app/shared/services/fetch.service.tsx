721088450ffc7ee471750ee9188716d5a5ac150a
MALIGLOK <karol.maliglowka@zf.com>
2024-10-21 21:39:48 +0200

    added new files

---
+++ src/SimpleCrud.WebReact/client-app/shared/services/fetch.service.tsx
@@ -0,0 +1,33 @@
+import {baseUrl} from "@/environment/environment";
+
+export async function get(route: string) {
+    try {
+        const response = await fetch(`${baseUrl}/${route}`, {
+            method: 'GET',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+        });
+        return response.json();
+    } catch (err) {
+        console.error(`GET error: ${err}`);
+    }
+}
+
+
+export async function post(route: string, body: any) {
+    const request = new Request(`${baseUrl}/${route}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+
+    try {
+        const response = await fetch(request);
+        const result = await response.json();
+        console.log("Success:", result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
