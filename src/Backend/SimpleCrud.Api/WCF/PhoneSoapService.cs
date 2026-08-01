c49324f885ce173de6c2f98c080dc425b0d5b1e4
Karol <karol_maliglowka@o2.pl>
2026-08-01 21:00:02 +0200

    refaktoring

--- src/Backend/SimpleCrud.Api/WCF/PhoneSoapService.cs
+++ src/Backend/SimpleCrud.Api/WCF/PhoneSoapService.cs
@@ -5,43 +5,27 @@ namespace SimpleCrud.Api.WCF;
 
 public class PhoneSoapService(IPhoneService phoneService) : IPhoneSoapContract
 {
-    public async Task<List<PhoneDto>>? GetPhones()
-    {
-        return await phoneService.GetAllPhones();
-    }
+    public async Task<List<PhoneDto>> GetPhones() =>
+        await phoneService.GetAllPhones();
 
-    public async Task<PhoneDto?> GetById(Guid phoneId)
-    {
-        return await phoneService.GetById(phoneId);
-    }
+    public async Task<PhoneDto> GetById(Guid phoneId) =>
+        await phoneService.GetById(phoneId);
 
-    public async Task<PhoneDto?> GetByName(string phoneName)
-    {
-        return await phoneService.GetByName(phoneName);
-    }
+    public async Task<PhoneDto> GetByName(string phoneName) =>
+        await phoneService.GetByName(phoneName);
 
-    public async Task<PhoneDto?> GetByNumber(string phoneNumber)
-    {
-        return await phoneService.GetByNumber(phoneNumber);
-    }
-    
-    public async Task<Guid> AddPhone(PhoneDto command)
-    {
-        return await phoneService.AddPhone(command);
-    }
-    
-    public async Task UpdatePhone(PhoneDto? command)
-    {
+    public async Task<PhoneDto> GetByNumber(string phoneNumber) =>
+        await phoneService.GetByNumber(phoneNumber);
+
+    public async Task<Guid> AddPhone(PhoneDto command) =>
+        await phoneService.AddPhone(command);
+
+    public async Task UpdatePhone(PhoneDto command) =>
         await phoneService.UpdatePhone(command);
-    }
-    
-    public async Task DeletePhone(Guid phoneId)
-    {
+
+    public async Task DeletePhone(Guid phoneId) =>
         await phoneService.DeletePhone(phoneId);
-    }
-    
-    public async Task DeleteManyPhones(List<Guid> phoneIds)
-    {
+
+    public async Task DeleteManyPhones(List<Guid> phoneIds) =>
         await phoneService.DeleteManyPhones(phoneIds);
-    }
 }
